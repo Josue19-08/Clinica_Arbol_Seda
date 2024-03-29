c43ee05f9be78caecc53b4decfa7eb0a2bd5edff
Josué <josuemarin2009@hotmail.com>
2024-03-29 13:45:42 -0600

    Se agrega nuevas páginas y se corrigen estilos

---
+++ js/politicas.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('botonDescargar').addEventListener('click', function () {
+        // Crear un enlace temporal
+        var enlace = document.createElement('a');
+        enlace.href = '../pdf/PoliticasPrivacidad.pdf';
+        // Especificar que el enlace sea descargado al hacer clic
+        enlace.setAttribute('download', 'PoliticasPrivacidad.pdf');
+        // Hacer clic en el enlace temporal
+        enlace.click();
+    });
+});
