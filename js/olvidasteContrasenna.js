65aaba24017e7dd960d4f4758eeeaa072f0893ab
Josué Araya Marín <104031367+Josue19-08@users.noreply.github.com>
2024-07-07 10:28:08 -0600

    Update password

--- js/olvidasteContrasenna.js
+++ js/olvidasteContrasenna.js
@@ -1,30 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
     var formularioRecuperacion = document.getElementById('formulario-recuperacion');
-    var mensaje = document.getElementById('mensaje');
-    var cedula = document.getElementById('cedula').value;
-
-    function validarCedula(cedula) {
-        // Validar que la cédula tenga el formato ##-####-####
-        var cedulaExpReg = /^\d{2}-\d{4}-\d{4}$/;
-        return cedulaExpReg.test(cedula);
-    }
+    var botonEnviar = document.getElementById('button');
 
     formularioRecuperacion.addEventListener('submit', function(event) {
         event.preventDefault();
-        var cedula = document.getElementById('cedula').value;
-        // Verificar si la cédula existe en la base de datos de usuarios
-        var usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-        var usuarioEncontrado = usuarios.find(function(u) {
-            return u.cedula === cedula;
-        });
-        if (usuarioEncontrado) {
-            // Aquí enviarías el correo electrónico al correo del usuario encontrado
-            alert('Se ha enviado un correo para restablecer la contraseña');
-        } else {
-            alert('No se encontró ningún usuario con el número de cédula digitado');
+        var email = document.getElementById('email').value;
+
+        // Validar formato de correo electrónico
+        if (!validarEmail(email)) {
+            alert('Por favor, ingresa un correo electrónico válido.');
+            return;
         }
+
+        // Enviar correo de recuperación
+        const emailParams = {
+            to_email: email,
+            to_name: 'Usuario', // Puedes personalizar esto según tus necesidades
+            reply_to: 'no-reply@clinicaseda.com'
+        };
+
+        emailjs.send('default_service', 'template_ev380ab', emailParams)
+            .then(function(response) {
+                console.log('Correo enviado con éxito!', response.status, response.text);
+                alert('Se ha enviado un correo para restablecer la contraseña.');
+            }, function(error) {
+                console.error('Error al enviar el correo:', error);
+                alert('Error al enviar el correo de recuperación de contraseña.');
+            });
     });
 
+    function validarEmail(email) {
+        var emailExpReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailExpReg.test(email);
+    }
 });
-
-
